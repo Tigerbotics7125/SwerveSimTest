d30a950daf8983a196faaca24da1ee41a94d2721
Jeffrey Morris <ffejcsirrom@gmail.com>
2022-06-11 22:29:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# TigerLibTemplate
+A template for projects using [TigerLib](https://github.com/Tigerbotics7125/TigerLib)
+---
+This is simply a command-based robot project with tigerlib preinstalled.
---
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2022, Tigerbotics' team members and all other contributors.
+ * Open source software; you can modify and/or share this software.
+ */
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+    /** Creates a new ExampleSubsystem. */
+    public ExampleSubsystem() {}
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+}
